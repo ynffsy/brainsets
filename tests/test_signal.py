4ebe3b75af4fc054b2a55730f83c210e9338b78b
Mehdi Azabou <36160333+mazabou@users.noreply.github.com>
2024-03-15 09:42:42 -0400

    fix unit tests (#162)
    * fix unit tests
    
    * update test workflow
    
    * rm pip downgrade from README

--- tests/test_signal.py
+++ tests/test_signal.py
@@ -1,6 +1,6 @@
 import numpy as np
 from kirby.data.signal import downsample_wideband, extract_bands, cube_to_long
-import torch
+
 
 def test_extract_bands():
     bands, ts, band_names = extract_bands(
@@ -11,15 +11,15 @@ def test_extract_bands():
     assert bands.shape[2] == len(band_names)
 
 def test_cube_to_long():
-    ts = np.arange(10)
+    ts = np.arange(10).astype(float)
     cube = np.zeros((1, 10, 2), dtype=int)
     cube[0, 6, 0] = 2
     cube[0, 5, 1] = 3
 
     trials, units = cube_to_long(ts, cube)
 
-    assert len(units.unit_name) == 2
+    assert len(units.id) == 2
     assert len(trials) == 1
     assert len(trials[0].timestamps) == 5
-    assert torch.all(torch.tensor([1, 1, 1, 0, 0]).eq(torch.tensor(trials[0].unit_index)))
-    assert torch.all(torch.tensor([5, 5, 5, 6, 6]).eq(torch.tensor(trials[0].timestamps)))
+    assert np.allclose(np.array([1, 1, 1, 0, 0]), trials[0].unit_index)
+    assert np.allclose(np.array([5, 5, 5, 6, 6]), trials[0].timestamps)
