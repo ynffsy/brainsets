c826321317a5d771e44245436b5bb135d91b31cd
ynffsy <fyin@caltech.edu>
2025-02-20 22:08:19 -0800

    Update prepare_data_batch_CSN.py

--- brainsets_pipelines/andersen_nih/prepare_data_batch_CSN.py
+++ brainsets_pipelines/andersen_nih/prepare_data_batch_CSN.py
@@ -12,54 +12,54 @@ data_config = {
         ],
         'nwb_data_dir': '/home/ynffsy/Desktop/andersen_lab/data/neurogalaxy/raw/andersen_nih/N1',
         'CenterOut': [
-            '20230818',
-            '20230824',
-            '20230901',
-            '20230908',
-            '20230929',
-            '20231208',
-            '20240104', # No assist 0 run
-            '20240112', # No assist 0 run
-            '20240117', 
-            '20240119', # No assist 0 run
-            '20240126', # No assist 0 run
-            '20240201', # No assist 0 run
-            '20240202', # No assist 0 run
-            '20240208', # No assist 0 run
-            '20240209', # No assist 0 run
-            '20240214', # No assist 0 run
-            '20240215', # No assist 0 run
-            '20240222', # No assist 0 run
-            '20240223', # No assist 0 run
-            '20240229', # No assist 0 run
-            '20240306', # No assist 0 run
-            '20240313', 
-            '20240419', 
-            '20240424', 
-            '20240502', # No assist 0 run
-            '20240509', # No assist 0 run
-            '20240516', # No assist 0 run
-            '20240517', # No assist 0 run
-            '20240528', 
-            '20240529', # No assist 0 run
-            '20240605', # No assist 0 run
-            '20240606', # No assist 0 run
-            '20240607', # No assist 0 run
-            '20240612', # No assist 0 run
-            '20240613', # No assist 0 run
-            '20240614', # No assist 0 run
-            '20240627', # No assist 0 run
-            '20240705', # No assist 0 run
-            '20240719', 
-            '20240731', # No assist 0 run
-            '20240815', # No assist 0 run
-            '20240829', 
-            '20240912', # No assist 0 run
-            '20240918', # No assist 0 run
-            '20240927',
-            '20241003', # No assist 0 run
-            '20241011', # No assist 0 run
-            '20241018',
+            # '20230818',
+            # '20230824',
+            # '20230901',
+            # '20230908',
+            # '20230929',
+            # '20231208',
+            # '20240104', # No assist 0 run
+            # '20240112', # No assist 0 run
+            # '20240117', 
+            # '20240119', # No assist 0 run
+            # '20240126', # No assist 0 run
+            # '20240201', # No assist 0 run
+            # '20240202', # No assist 0 run
+            # '20240208', # No assist 0 run
+            # '20240209', # No assist 0 run
+            # '20240214', # No assist 0 run
+            # '20240215', # No assist 0 run
+            # '20240222', # No assist 0 run
+            # '20240223', # No assist 0 run
+            # '20240229', # No assist 0 run
+            # '20240306', # No assist 0 run
+            # '20240313', 
+            # '20240419', 
+            # '20240424', 
+            # '20240502', # No assist 0 run
+            # '20240509', # No assist 0 run
+            # '20240516', # No assist 0 run
+            # '20240517', # No assist 0 run
+            # '20240528', 
+            # '20240529', # No assist 0 run
+            # '20240605', # No assist 0 run
+            # '20240606', # No assist 0 run
+            # '20240607', # No assist 0 run
+            # '20240612', # No assist 0 run
+            # '20240613', # No assist 0 run
+            # '20240614', # No assist 0 run
+            # '20240627', # No assist 0 run
+            # '20240705', # No assist 0 run
+            # '20240719', 
+            # '20240731', # No assist 0 run
+            # '20240815', # No assist 0 run
+            # '20240829', 
+            # '20240912', # No assist 0 run
+            # '20240918', # No assist 0 run
+            # '20240927',
+            # '20241003', # No assist 0 run
+            # '20241011', # No assist 0 run
+            # '20241018',
         ],
     },
     'N2': {
@@ -72,7 +72,18 @@ data_config = {
         ],
         'nwb_data_dir': '/home/ynffsy/Desktop/andersen_lab/data/neurogalaxy/raw/andersen_nih/N2',
         'CenterOut': [
+            '20240118',
+            '20240123',
+            '20240126',
+            '20240130',
+            '20240201',
+            '20240206',
+            '20240208',
+            '20240213',
+            '20240215',
+            '20240221',
             '20240222',
+            '20240228',
             '20240229',
             '20240306',
             '20240308',
@@ -106,7 +117,16 @@ data_config = {
             '20241015',
             '20241022',
             '20241105',
+            '20241106',
+            '20241113',
+            '20241119',
             '20241211',
+            '20241212',
+            '20241224',
+            '20250107',
+            '20250114',
+            '20250128',
+            '20250211',
         ]
     }
 }
